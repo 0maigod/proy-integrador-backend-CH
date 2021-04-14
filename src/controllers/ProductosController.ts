28ddefee38d1402d512d27a3dc702d11a47c608d
0maigod <easterisco@gmail.com>
2021-04-14 11:05:04 -0300

    Refactorizado - saque socketio y el archivo de routes lo cambien por un controller

---
+++ src/controllers/ProductosController.ts
@@ -0,0 +1,99 @@
+import { Router } from 'express';
+import DBProducto from '../models/Producto';
+import { Producto } from '../producto';
+
+const router = Router();
+const isAdmin = true;
+
+router
+    .get('/:id?', async (req, res) => {
+        const { id } = req.params;
+        let reqProds;
+        if (!id) {
+            reqProds = await DBProducto.find();
+            return res.status(200).json(reqProds);
+        }
+        reqProds = await DBProducto.find({ _id: id });
+        if (reqProds.length == 0) {
+            return res.send(`{error: 'producto no encontrado'}`);
+        }
+        return res.status(200).json(reqProds);
+    })
+    .get('/precio/:min&&:max', async (req, res) => {
+        let min = req.params.min;
+        let max = req.params.max;
+        if (!min || max) {
+            console.log('falta un precio');
+            return;
+        }
+        console.log('precio minimo ' + min);
+        console.log('precio maximo ' + max);
+    })
+    .post('/', async (req, res) => {
+        if (isAdmin) {
+            let timestamp = Date.now();
+            const { nombre, precio, foto, descripcion, codigo, stock } = req.body;
+            const prod = new DBProducto({
+                timestamp,
+                nombre,
+                precio: parseInt(precio),
+                foto,
+                descripcion,
+                codigo,
+                stock: parseInt(stock)
+            });
+            await prod.save();
+            console.log('Producto agregado a DB');
+            res.status(200).json(prod);
+            return;
+        }
+        res.send(`{ error : -1, descripcion: ruta '/productos' método 'agregar' no autorizado }`);
+    })
+    .put('/:id?', async (req, res) => {
+        if (isAdmin) {
+            const { id } = req.params;
+            let prod = await DBProducto.find({ _id: id });
+            if (prod.length == 0) {
+                res.send(`{error: 'producto no encontrado'}`);
+                return;
+            }
+            let timestamp = Date.now();
+            const { nombre, precio, foto, descripcion, codigo, stock } = req.body;
+            let prodNuevo: Producto = {
+                id: id,
+                timestamp,
+                nombre,
+                precio: parseInt(precio),
+                foto,
+                descripcion,
+                codigo,
+                stock: parseInt(stock)
+            };
+            await DBProducto.updateOne({ _id: id }, prodNuevo);
+            console.log('Producto modificado en DB');
+            res.status(200).json(prod);
+
+            return;
+        }
+        res.send(`{ error : -1, descripcion: ruta '/productos' método 'modificar' no autorizado }`);
+    })
+    .delete('/:id?', async (req, res) => {
+        if (isAdmin) {
+            const { id } = req.params;
+            if (!id) {
+                res.send(`{ error : -1, descripcion: debe ingresar el ID del producto }`);
+                return;
+            }
+            let prod = await DBProducto.find({ _id: id });
+            if (prod.length == 0) {
+                res.send(`{error: 'producto no encontrado'}`);
+                return;
+            }
+            await DBProducto.deleteOne({ _id: id });
+            res.send(prod);
+            return;
+        }
+        res.send(`{ error : -1, descripcion: ruta '/productos' método 'borrar' no autorizado }`);
+    });
+
+export default router;
---
+++ src/databases/mongo.db.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+async function database() {
+    try {
+        await mongoose.connect('mongodb+srv://cluster0.wekjp.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            user: 'omero',
+            pass: 'Urkrb9RrNJi6vuZ',
+            keepAlive: true,
+            useUnifiedTopology: true
+        });
+        console.log('>>> Database connected');
+    } catch (e: any) {
+        console.log(e);
+    }
+}
+
+export default database;
