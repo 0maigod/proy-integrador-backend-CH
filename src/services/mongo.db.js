109fa6ce713348caea46ac9631b0792cb561f809
0maigod <easterisco@gmail.com>
2021-06-04 16:15:37 -0300

    refacto 02

--- src/services/mongo.db.js
+++ src/services/mongo.db.js
@@ -13,4 +13,27 @@ async function database() {
     }
 }
 
-module.exports = database;
+module.exports = {database};
+// //----------------------------------
+
+// const mongoose = require('mongoose');
+// mongoose.Promise = global.Promise;
+// let isConnected;
+
+// module.exports = database = (databaseName = process.env.DBNAME) => {
+//   if (isConnected) {
+//     console.log('=> usando la coneccion a la base de datos');
+//     return Promise.resolve();
+//   }
+
+//   console.log('>>> Database connected');
+//   const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${encodeURIComponent(
+//     process.env.MONGO_PSW)}@cluster0.6bf5k.mongodb.net/${databaseName}?retryWrites=true`;
+//   return mongoose.connect(uri, { 
+//             useNewUrlParser: true, 
+//             keepAlive: true,
+//             useUnifiedTopology: true
+//         }).then((db) => {
+//                 isConnected = db.connections[0].readyState;
+//         });
+// };
